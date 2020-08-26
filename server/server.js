69e90a89e34096e887773007ebd97a1801583ae3
chengmy <1208348567@qq.com>
2020-08-26 18:42:33 +0800

    功能提交

--- server/server.js
+++ server/server.js
@@ -27,11 +27,11 @@ app.use(cookieParser());
 const PORT = 8088;
 
 
-app.use('*',function(req,res,next){
+app.use('/req',function(req,res,next){
   res.header('Access-Control-Allow-Origin', '*');//的允许所有域名的端口请求（跨域解决）
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
-  // res.header('Content-Type', 'application/json;charset=utf-8');
+  res.header('Content-Type', 'application/json;charset=utf-8');
   next();
 }); 
 
