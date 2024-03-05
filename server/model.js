ec105fb3d462930cb88f1762890a6d9142008963
程敏英 <1208348567@qq.com>
2024-03-05 14:53:34 +0800

    修改mongoDB数据库连接地址

--- server/model.js
+++ server/model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //连接mongodb
-const DB_URL = "mongodb://chengmy:cheng76735206@119.45.23.113:27017/website";
+const DB_URL = "mongodb://ubuntu:Cheng76735206@110.40.213.74:27017/website";
 mongoose.connect(DB_URL);
 mongoose.connection.on('connected', function() {
     console.log("mongoDB连接成功");
